ae10ac6097a6fbeaadd88fe6bfc6a57373f33e00
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-10-14 14:54:26 -0700

    init

--- main.py
+++ main.py
@@ -10,26 +10,11 @@ ZIPCODE = 92126
 if __name__ == '__main__':
     args = parser.parse_args()
     
-    try:
-        with open('forecast.json') as f:
-            yesterday_forecast = json.load(f)
-        if yesterday_forecast['daily_will_it_rain'] or yesterday_forecast['daily_chance_of_rain'] > 30:
-            requests.post(args.discord, data={
-                'content': f"it was predicted to {'rain' if yesterday_forecast['daily_will_it_rain'] else 'not rain'} with a chance of {yesterday_forecast['daily_chance_of_rain']}",
-                'username': "WeatherBot"
-            })
-    except:
-        pass
-
-
     req = requests.get(f'https://api.weatherapi.com/v1/forecast.json?key={args.api}&q={ZIPCODE}&days=1&aqi=no&alerts=no')
     result = req.json()
-    if 'rain' in result['current']['condition']['text'].lower():
+    today = result['forecast']['forecastday'][0]['day']
+    if today['daily_will_it_rain'] or today['daily_chance_of_rain'] > 30:
         requests.post(args.discord, data={
-        'content': 'it rains today!!!',
-        'username': "WeatherBot"
-    })
-    tomorrow = result['forecast']['forecastday'][0]['day']
-    
-    with open('forecast.json', 'w') as f:
-        json.dump(result, f)
+            'content': f"it is predicted to {'rain' if today['daily_will_it_rain'] else 'not rain'} today with a chance of {today['daily_chance_of_rain']}%.",
+            'username': "WeatherBot"
+        })
